47508acff3e8a90410aed46a2cf1d0a33d59d04b
tauan-longaretti <tauanlongaretti@gmail.com>
2020-02-20 01:10:49 -0500

    created basic component for updating movie

--- client/src/Movies/UpdateMovieForm.js
+++ client/src/Movies/UpdateMovieForm.js
@@ -2,9 +2,29 @@ import React from "react";
 
 const UpdateMovieForm = () => {
     return (
-        <div>
-            <p>Update Movie Form</p>
-        </div>
+        <form>
+            <label>Movie Name:</label>
+            <input 
+              type="text"
+              name="movie"
+            />
+            <label>Director:</label>
+            <input 
+              type="text"
+              name="director"
+            />
+            <label>Metascore:</label>
+            <input 
+              type="text"
+              name="metascore"
+            />
+            <label>Actors:</label>
+            <input 
+              type="text"
+              name="actors"
+            />
+            <button>Submit</button>      
+        </form>
     )
 }
 
